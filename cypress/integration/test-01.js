198f03ef8388c40aa95cdc7807a338b7917d0e52
Fadel Nararia Rahman <47631524+fadelrahman31@users.noreply.github.com>
2020-06-20 07:57:06 +0700

    updated test case

--- cypress/integration/test-01.js
+++ cypress/integration/test-01.js
@@ -1,7 +1,7 @@
-describe("Test Quipper Home Page - Dashboard", () => {
+describe("Test Quipper Home Page", () => {
     it('#1 Successfully Loads', () => {
         cy.visit('https://www.quipper.com/id/')
-    }),
+    }), 
     it("#2 Showing the Correct Components", () => {
         cy.contains('Untuk Siswa')
         cy.contains('Untuk Guru')
@@ -10,15 +10,26 @@ describe("Test Quipper Home Page - Dashboard", () => {
         cy.contains('Login')
         cy.contains('Daftar Sekarang')
     }),
-    it('#3 Login to Quipper Video', () => {
+    it('#3 Move to Login Page', () => {
         cy.contains('Login').click()
         cy.url().should('include', '/en/login')
-        //cy.get('.TextInput--3Y0H3.Login__Input--26jGQ.form-control--Ijqjw')
-        cy.get('[aria-label="Username or email"]')
-          .type('fadelrahman10')
-        cy.get('[aria-label="Password"')
-          .type('nararia3-1')
-        cy.contains('Log In').click()
-        cy.url().should('include', '/dashboard')
     })
+}),
+describe("Test Sign Up Page", () => {
+  it("#1 Successfully Loads", () => {
+      cy.visit('https://subscribe.quipper.com/signup/id')
+  }),
+  it('#2 Showing The Correct Components', () => {
+      cy.contains('Select your location')
+      cy.get('[name= "country"]')
+      cy.get('.fb-button')
+      cy.get('[name= "firstName"]')
+      cy.get('[name= "lastName"]')
+      cy.get('[name= "gradeLevel"]')
+      cy.get('[name = "phoneNumber"]')
+      cy.get('[name = "email"]')
+      cy.get('[name = "username"]')
+      cy.get('[name = "password"]')
+      cy.get('[name = "passwordConfirmation"]')
+  })
 })
