784830f87898d98f211b1ba6d6648ae37de38e26
Fadel Nararia Rahman <47631524+fadelrahman31@users.noreply.github.com>
2020-06-22 13:59:35 +0700

    updates on subscribe process test

--- cypress/integration/id-subscribeProcess.js
+++ cypress/integration/id-subscribeProcess.js
@@ -1,5 +1,11 @@
 describe("Test Subscribe Process", () => {
     it("#1 Login to Test Account", () => {
-        
+        cy.visit(Cypress.env('quipper_login_page'))
+        cy.get('[aria-label="Username or email"]')
+          .type(Cypress.env('uname_testing'))
+        cy.get('[aria-label="Password"')
+          .type(Cypress.env('pword_testing'))
+        cy.contains('Log In').click()
+        cy.url().should('include', '/dashboard')
     })
 })
