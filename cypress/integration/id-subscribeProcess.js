8eeae3d53b6416058b7cfe6e762d74fba959007d
Fadel Nararia Rahman <47631524+fadelrahman31@users.noreply.github.com>
2020-06-22 15:05:43 +0700

    updates

--- cypress/integration/id-subscribeProcess.js
+++ cypress/integration/id-subscribeProcess.js
@@ -1,5 +1,5 @@
 describe("Test Subscribe Process", () => {
-    it("#1 Login to Test Account", () => {
+    beforeEach(() => {
         cy.visit(Cypress.env('quipper_login_page'))
         cy.get('[aria-label="Username or email"]')
           .type(Cypress.env('uname_testing'))
@@ -7,5 +7,24 @@ describe("Test Subscribe Process", () => {
           .type(Cypress.env('pword_testing'))
         cy.contains('Log In').click()
         cy.url().should('include', '/dashboard')
+    })
+
+    // it("#1 Login to Test Account", () => {
+    //     cy.visit(Cypress.env('quipper_login_page'))
+    //     cy.get('[aria-label="Username or email"]')
+    //       .type(Cypress.env('uname_testing'))
+    //     cy.get('[aria-label="Password"')
+    //       .type(Cypress.env('pword_testing'))
+    //     cy.contains('Log In').click()
+    //     cy.url().should('include', '/dashboard')
+    // }),
+    it("#1 Load Page Successfully", () => {
+        cy.visit(Cypress.env('quipper_dashboard'))
+    }),
+    it("#2 Dashboard Showing Correct Components", () => {
+        cy.contains('Home')
+        cy.contains('Course List')
+        cy.contains('My Courses')
+        cy.get('.Avatar--2txd-')
     })
 })
