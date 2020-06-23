b625bf78150833de068a0ce1c8c6bb5dc6512fe2
Fadel Nararia Rahman <47631524+fadelrahman31@users.noreply.github.com>
2020-06-23 12:32:48 +0700

    progress on SubscribeProcess test cases

--- cypress/integration/id-subscribeProcess.js
+++ cypress/integration/id-subscribeProcess.js
@@ -9,15 +9,6 @@ describe("Test Subscribe Process", () => {
         cy.url().should('include', '/dashboard')
     })
 
-    // it("#1 Login to Test Account", () => {
-    //     cy.visit(Cypress.env('quipper_login_page'))
-    //     cy.get('[aria-label="Username or email"]')
-    //       .type(Cypress.env('uname_testing'))
-    //     cy.get('[aria-label="Password"')
-    //       .type(Cypress.env('pword_testing'))
-    //     cy.contains('Log In').click()
-    //     cy.url().should('include', '/dashboard')
-    // }),
     it("#1 Load Page Successfully", () => {
         cy.visit(Cypress.env('quipper_dashboard'))
     }),
@@ -25,6 +16,30 @@ describe("Test Subscribe Process", () => {
         cy.contains('Home')
         cy.contains('Course List')
         cy.contains('My Courses')
-        cy.get('.Avatar--2txd-')
+        cy.get('.Avatar--2txd-').click()
+        cy.contains('Profile')
+        cy.contains('Subscriptions')
+        cy.contains('Search campuses')
+        cy.contains('Logout')
+        cy.get('.Avatar--2txd-').click()
+        //Upsell Banner
+        cy.get('.UpsellBanner--2x5p2')
+        cy.contains('Pilih Paket')
+        cy.contains('Coba Gratis Dulu')
+        //Notification Banner
+        cy.get('.DashboardModuleTemplate__Banner--1GLG-')
+        cy.contains('Subscribe now')
+    }),
+    it("#3 Avatar Button Menu Functionality", () => {
+        cy.get('.Avatar--2txd-').click()
+        cy.contains('Profile').click()
+        cy.url().should('include', '/en/settings/profile')
+        cy.contains('Home').click()
+        cy.url().should('include', '/dashboard')
+        cy.get('.Avatar--2txd-').click()
+        cy.contains('Subscriptions').click()
+        cy.url().should('include', '/plans')
+        cy.contains('Back to learning app').click()
+        cy.url().should('include', '/dashboard')
     })
 })
